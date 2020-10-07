2118539d0a5091c604de93a9b49880bead2d0634
abc <abc@example.com>
2020-10-07 17:39:39 +0530

    Uploading SpringBoot application to AWS for CodeCommitDeploy

--- Demo/src/Sai.java
+++
@@ -1,8 +0,0 @@
-
-public class Sai {
-
-	public static void main(String[] args) {
-		System.out.println("xxxbb  xxx   xxxxxxxxxx");
-	}
-
-}
--- Test/src/xx.java
+++
@@ -1,8 +0,0 @@
-
-public class xx {
-
-	public static void main(String[] args) {
-		System.out.println("nani.....");
-	}
-
-}
